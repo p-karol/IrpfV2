6b1d70d9c33e54a85118503392fd8e2b553d8514
p-karol <priscilla.karolczak@gmail.com>
2019-11-17 18:42:25 +0000

    Refatorados classes contribuinte e validadorContribuinte

--- src/negocio/Contribuinte.java
+++ src/negocio/Contribuinte.java
@@ -3,33 +3,51 @@ package negocio;
 public class Contribuinte {
 
     private String nome;
-    private String telefone;
-    private char sexo;
+    private String cpf;
+    private int Idade;
+    private int numeroDependentes;
+    private double contrubuicaoPrevidenciaria;
+    private double totalRendimentos;
 
-    public Contribuinte(String umNome, String umTelefone, boolean masculino) {
-        nome = umNome;
-        telefone = umTelefone;
-        if (masculino) {
-            sexo = 'M';
-        } else {
-            sexo = 'F';
-        }
+    public Contribuinte(String nome, String cpf, int idade, int numDependentes, double contrubuicaoPrevidenciaria, double totalRendimentos) {
+        this.nome = nome;
+        this.cpf = cpf;
+        Idade = idade;
+        this.numeroDependentes = numeroDependentes;
+        this.contrubuicaoPrevidenciaria = contrubuicaoPrevidenciaria;
+        this.totalRendimentos = totalRendimentos;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public String getTelefone() {
-        return telefone;
+    public String getCpf() {
+        return cpf;
     }
-    
-    public char getSexo() {
-        return sexo;
+
+    public int getIdade() {
+        return Idade;
+    }
+
+    public int getNumeroDependentes() {
+        return numeroDependentes;
+    }
+
+    public double getContrubuicaoPrevidenciaria() {
+        return contrubuicaoPrevidenciaria;
+    }
+
+
+    public double getTotalRendimentos() {
+        return totalRendimentos;
     }
 
     @Override
     public String toString() {
-        return "[" + nome + "," + telefone + "," + sexo + "]";
+        return "Contribuinte [Idade=" + Idade + ", contrubuicaoPrevidenciaria=" + contrubuicaoPrevidenciaria + ", cpf=" + cpf
+                + ", nome=" + nome + ", numDependentes=" + numeroDependentes + ", totalRendimentos=" + totalRendimentos
+                + "]";
     }
+
 }
